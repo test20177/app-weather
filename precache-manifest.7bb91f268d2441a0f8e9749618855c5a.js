5360397e99be8e862a7e8afbbdbac9e9b7be3050
kit <kit@kit.com>
2020-03-31 11:03:06 +0100

    Updates

---
+++ precache-manifest.7bb91f268d2441a0f8e9749618855c5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a3de3860f74f2350ddac35f9ca7f4b",
+    "url": "/app-weather/index.html"
+  },
+  {
+    "revision": "1f1ca5bc297e89bdf9df",
+    "url": "/app-weather/static/css/main.7583533b.chunk.css"
+  },
+  {
+    "revision": "2cd56b36daf142002565",
+    "url": "/app-weather/static/js/2.240c2cb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-weather/static/js/2.240c2cb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1ca5bc297e89bdf9df",
+    "url": "/app-weather/static/js/main.5377c8b0.chunk.js"
+  },
+  {
+    "revision": "d57de061978bf06962f0",
+    "url": "/app-weather/static/js/runtime-main.354150b3.js"
+  }
+]);
